fa3c1bd35a1685832d998f1bfbdbdec2eafffce1
TosatXu <zihanhsu83@gmail.com>
2025-05-06 12:52:50 +1000

    End of prac 06/06/2025

---
+++ Assets/Tiled/TilemapGame/TinyTown_Tileset_2.tsx
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyTown_Tileset_2" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="kenney_tiny-town/Tilemap/tilemap_packed.png" width="192" height="176"/>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.81818" y="5.27273" width="11.7273" height="9.72727"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.18182" y="4.18182" width="11.9091" height="10.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="2" width="8" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.18182" y="-0.363636" width="8.18182" height="14">
+    <polygon points="0,0 8.18182,0 8.18182,14 0,14"/>
+   </object>
+   <object id="2" x="-4.44089e-16" y="5.27273" width="12.1818" height="8.36364"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="4.90909" width="12" height="9.09091"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="Hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4.81818" width="10.9091" height="11.1818"/>
+  </objectgroup>
+ </tile>
+</tileset>
